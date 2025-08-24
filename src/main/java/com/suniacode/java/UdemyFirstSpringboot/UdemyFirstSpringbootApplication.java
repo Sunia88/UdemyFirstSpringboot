7e51b6472cb55d096a0ff33e9e72a4930a08df26
Sunia88 <sunia.raharja@gmail.com>
2025-08-24 18:52:42 +0800

    first commit

---
+++ src/main/java/com/suniacode/java/UdemyFirstSpringboot/UdemyFirstSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.suniacode.java.UdemyFirstSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyFirstSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyFirstSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suniacode/java/UdemyFirstSpringboot/UdemyFirstSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suniacode.java.UdemyFirstSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyFirstSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
